4ea7791e6d7768651ed997df80d41089b39ded77
tim <tim@biglenovo.lan>
2023-11-02 21:00:10 -0500

    ota url and building gapps (but its microg)

--- aosp.mk
+++ aosp.mk
@@ -1,4 +1,4 @@
 $(call inherit-product, vendor/ponces/config/common.mk)
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.system.ota.json_url=https://raw.githubusercontent.com/ponces/treble_build_aosp/android-14.0/ota.json
+    ro.system.ota.json_url=https://tvall43.ddns.net/treble_build_aosp/ota.json
